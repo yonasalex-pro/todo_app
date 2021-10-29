b14b995ea2ffcfcf3567f5cca3a17ba9623b4b6f
Yonas Alemayehu <yonasalex.pro@gmail.com>
2021-10-29 19:27:12 +0300

    add some theme properties and common styles

--- lib/ui/themes.dart
+++ lib/ui/themes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 const Color blue = Color(0xFF4E5AE8);
 const Color yellow = Color(0xFFFFB746);
@@ -11,11 +12,21 @@ class Themes {
   static final ThemeData light = ThemeData(
     brightness: Brightness.light,
     appBarTheme: const AppBarTheme(backgroundColor: white, elevation: 0),
-    canvasColor: white,
+    backgroundColor: white,
+    scaffoldBackgroundColor: white,
   );
   static final ThemeData dark = ThemeData(
     brightness: Brightness.dark,
     appBarTheme: const AppBarTheme(backgroundColor: darkGrey, elevation: 0),
-    canvasColor: darkGrey,
+    backgroundColor: darkGrey,
+    scaffoldBackgroundColor: darkGrey,
+  );
+}
+
+TextStyle get subHeadingStyle {
+  return TextStyle(
+    color: Get.isDarkMode ? Colors.grey[400] : Colors.grey.withOpacity(0.7),
+    fontSize: 13,
+    fontWeight: FontWeight.bold,
   );
 }
